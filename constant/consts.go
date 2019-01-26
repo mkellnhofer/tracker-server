ccf76761247a1420d0e4860165d2fecf0dbe6348
Matthias Kellnhofer <matthias@kellnhofer.com>
2019-01-27 00:44:23 +0100

    Set version to 1.0.0

--- constant/consts.go
+++ constant/consts.go
@@ -1,7 +1,7 @@
 package constant
 
 const (
-	AppVersion string = "1.0.0-alpha"
+	AppVersion string = "1.0.0"
 
 	DbDateFormat  string = "2006-01-02 15:04:05"
 	ApiDateFormat string = "2006-01-02T15:04:05Z"
